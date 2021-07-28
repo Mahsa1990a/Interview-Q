07cd22984dffd45095912e7d300aa5bc7765307d
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-07-28 16:10:07 +0000

    Q2

---
+++ react.js
@@ -0,0 +1,19 @@
+function Banner() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    console.log("Count is updated");
+  });
+
+  return (
+    <div>
+      <button onClick={() => setCount(count + 1)}>State: {count}</button>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+    </div>
+  );
+}
